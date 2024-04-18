beb73a3ffeae9e4946e185eefb0223cd94f4702a
tpqls774 <tpqls774@dgsw.hs.kr>
2024-04-18 19:24:14 +0900

    baekjoon.5597번

--- baekjoon.py
+++ baekjoon.py
@@ -1,5 +1,7 @@
-n = int(input())
-num = 1
-for i in range(1, n+1):
-    num *= i
-print(num)
+num = []
+for i in range(1, 29):
+    num.append(int(input()))
+
+for j in range(1, 31):
+    if j not in num:
+        print(j)
