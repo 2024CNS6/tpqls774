21bf8400b02256c111749bda1dde9e16b4b0dfe9
tpqls774 <tpqls774@dgsw.hs.kr>
2024-04-19 16:11:57 +0900

    baekjoon.9086번

--- baekjoon.py
+++ baekjoon.py
@@ -1,8 +1,4 @@
 t = int(input())
-s = []
 for i in range(t):
-    a, b = input().split()
-    for j in range(len(b)):
-        s.append(int(a)*b[j])
-    print(f"{''.join(s)}")
-    s = []
+    a = input()
+    print(f"{a[0]}{a[-1]}")
