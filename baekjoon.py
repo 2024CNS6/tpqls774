c713b28a1b15abf61c4c544d4bb40abcb2fa4d70
tpqls774 <tpqls774@dgsw.hs.kr>
2024-04-19 15:55:14 +0900

    baekjoon.2675번

--- baekjoon.py
+++ baekjoon.py
@@ -1,2 +1,8 @@
-a, b = map(int, input().split())
-print(a**2-b**2)
+t = int(input())
+s = []
+for i in range(t):
+    a, b = input().split()
+    for j in range(len(b)):
+        s.append(int(a)*b[j])
+    print(f"{''.join(s)}")
+    s = []
