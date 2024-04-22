ab10eb33c1523b65c6c29ffbf1bd58394b7cab5c
tpqls774 <tpqls774@dgsw.hs.kr>
2024-04-22 19:54:44 +0900

    02.[EASY] 학생 성적 분류 및 정렬 시스템

--- baekjoon.py
+++ baekjoon.py
@@ -1,14 +1,20 @@
-t = int(input())
-count = 0
-double = 0
-for i in range(t):
-    a = input()
-    for j in range(len(a)):
-        if a[j] == "O":
-            double += 1
-            count += double
-        elif a[j] == "X":
-            double = 0
-    print(count)
-    count = 0
-    double = 0
+n = int(input())
+li = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    ss = ""
+    if score >= 90:
+        ss += "A"
+    elif score >= 80:
+        ss += "B"
+    elif score >= 70:
+        ss += "C"
+    elif score >= 60:
+        ss += "D"
+    else:
+        ss += "F"
+    li.append((name, ss, score))
+li.sort(key=lambda x : (x[2], x[0]), reverse=True)
+for i in li:
+    print(i[0], i[1])
