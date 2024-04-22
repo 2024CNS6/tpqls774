d7422bff914e128de840ef57872d0cb208b07edf
tpqls774 <tpqls774@dgsw.hs.kr>
2024-04-22 20:38:39 +0900

    06.[EASY] 모글리 영어 학습

--- baekjoon.py
+++ baekjoon.py
@@ -1,20 +1,6 @@
-n = int(input())
-li = []
-for i in range(n):
-    name, score = input().split()
-    score = int(score)
-    ss = ""
-    if score >= 90:
-        ss += "A"
-    elif score >= 80:
-        ss += "B"
-    elif score >= 70:
-        ss += "C"
-    elif score >= 60:
-        ss += "D"
-    else:
-        ss += "F"
-    li.append((name, ss, score))
-li.sort(key=lambda x : (x[2], x[0]), reverse=True)
-for i in li:
-    print(i[0], i[1])
+s = list(input().split())
+order = 0
+for i in range(len(s)):
+	order += ord(s[i][0]) - ord('a') + 1
+print(len(s))
+print(order)
