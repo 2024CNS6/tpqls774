33d6aebc1eee41e65b36db99652305e2d4e73e15
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-02 21:15:14 +0900

    07.[NORMAL] 티비의 크기는?

--- CodeUp.py
+++ CodeUp.py
@@ -1,5 +1,3 @@
-a, b, c = map(int, input().split())
-d = 1
-while d%a!=0 or d%b!=0 or d%c!=0 :
-    d += 1
-print(d)
+d,h,w = map(int,input().split())
+r = d/(h**2+w**2)**0.5
+print(int(h*r),int(w*r))
