577e8dace20b58eac0ed9fbec534d2119bc19c4c
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-07 20:34:14 +0900

    baekjoon.31798번

--- baekjoon.py
+++ baekjoon.py
@@ -1,15 +1,11 @@
-div = []
-count = 1
-a = 0
-
-for i in range(10):
-    num = int(input())
-    na = num % 42
-    div.append(na)
-div.sort()
-
-for n in range(9):
-    if div[n] < div[n+1]:
-        count += 1
-
-print(count)
+a, b, c = map(int, input().split())
+if a == 0:
+    a += c*c - b
+    print(a)
+elif b == 0:
+    b += c*c - a
+    print(b)
+else:
+    for i in range(10**4):
+        if a+b == i*i:
+            print(i)
