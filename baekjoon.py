d3a8abd6ccf36bff9d2b38bdc3c73358a38f650a
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-08 20:53:48 +0900

    baekjoon.1110번

--- baekjoon.py
+++ baekjoon.py
@@ -1,9 +1,17 @@
-a, b = map(int, input().split())
-
-re_a = int(str(a)[::-1])
-re_b = int(str(b)[::-1])
-
-if re_a > re_b:
-    print(re_a)
-else:
-    print(re_b)
+n = int(input())
+new = n
+cnt = 0
+while True:
+    a = new//10   
+    b = new - a*10
+    sum = a+b
+    if sum >= 10:
+        c = sum//10
+        d = sum - c*10
+        new = b*10 + d
+    else:
+        new = b*10 + sum
+    cnt += 1
+    if new == n:
+        break
+print(cnt)
