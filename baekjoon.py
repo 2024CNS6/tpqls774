367943a767b72386785d919d8647934e47c2b66d
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-13 20:53:00 +0900

    baekjoon.5543번

--- baekjoon.py
+++ baekjoon.py
@@ -1,5 +1,7 @@
-s = input()
-arr = [-1] * 26
-for i in s:
-    arr[ord(i)-97] = s.index(i)
-print(*arr)
+buger = []
+drink = []
+for i in range(3):
+    buger.append(int(input()))
+for i in range(2):
+    drink.append(int(input()))
+print(min(buger)+min(drink) -50)
