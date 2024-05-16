feb1c8ea8cda8563dea268e93bc1c483b30c0697
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-16 15:23:06 +0900

    baekjoon.1157번

--- baekjoon.py
+++ baekjoon.py
@@ -1,5 +1,17 @@
-a, b, v = map(int, input().split())
-if (v-b)%(a-b) == 0:
-    print((v-b)//(a-b))
+al = [0 for i in range(26)]
+s_1 = input()
+s_2 = s_1.lower()
+max_count = []
+
+for i in range(len(s_2)):
+    al[ord(s_2[i])-97] += 1
+
+for i in range(26):
+    if al[i] == max(al):
+        max_count.append(max(al))
+        
+if len(max_count) > 1:
+    print('?')
 else:
-    print((v-b)//(a-b)+1)
+    s_max = chr(al.index(max(al)) + 97)
+    print(s_max.upper())
