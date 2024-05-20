cad08da690e5c4dd724644b41257000d9996fba1
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-20 20:19:17 +0900

    baekjoon.1259번[팰린드롬수]

--- baekjoon.py
+++ baekjoon.py
@@ -1,18 +1,9 @@
-n = int(input())
-count = 0
-num = list(map(int, input().split()))
-
-for x in num:
-    if x == 1:
-        continue
-    is_prime = True
-    i = 2
-    while i * i <= x:
-        if x % i == 0:
-            is_prime = False
-            break
-        i += 1
-    if is_prime:
-        count += 1
-
-print(count)
+s = input()
+while True:
+    if s == '0':
+        break
+    if s == s[::-1]:
+        print('yes')
+    else:
+        print('no')
+    s = input()
