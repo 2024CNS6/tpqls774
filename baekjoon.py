f4f1423c3d3dca3e8360f30095f03a038d478370
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-24 15:29:26 +0900

    baekjoon.1316번[그룹 단어 체커]

--- baekjoon.py
+++ baekjoon.py
@@ -1,9 +1,20 @@
-s = input()
-while True:
-    if s == '0':
-        break
-    if s == s[::-1]:
-        print('yes')
-    else:
-        print('no')
+n = int(input())
+word = ""
+word_cnt = 0
+cnt = 0
+
+for i in range(n):
     s = input()
+    for j in range(len(s)):
+        if j == 0:
+            pass
+        elif s[j] == s[j-1]:
+            word_cnt += 1
+        elif s[j] not in word:
+            word_cnt += 1
+        word += s[j]
+    if word_cnt == len(s)-1:
+        cnt += 1
+    word_cnt = 0
+    word = ""
+print(cnt)
