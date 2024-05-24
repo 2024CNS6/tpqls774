bd8a6a8aa5e236b9f75c8cdc906eba4d915558b5
tpqls774 <tpqls774@dgsw.hs.kr>
2024-05-24 15:41:17 +0900

    baekjoon.2941번[크로아티아 알파벳]

--- baekjoon.py
+++ baekjoon.py
@@ -1,20 +1,23 @@
-n = int(input())
-word = ""
-word_cnt = 0
-cnt = 0
+s = list(input())
+length = len(s)
+for i in range(len(s)):
+    if i == 0:
+        continue
+    if s[i-1] == 'c' and s[i] == '=':
+        length -= 1
+    elif s[i-1] == 'c' and s[i] == '-':
+        length -= 1
+    elif s[i-2] == 'd' and s[i-1] == 'z' and s[i] == '=':
+        length -= 2
+    elif s[i-1] == 'd' and s[i] == '-':
+        length -= 1
+    elif s[i-1] == 'l' and s[i] == 'j':
+        length -= 1
+    elif s[i-1] == 'n' and s[i] == 'j':
+        length -= 1
+    elif s[i-1] == 's' and s[i] == '=':
+        length -= 1
+    elif s[i-1] == 'z' and s[i] == '=':
+        length -= 1
 
-for i in range(n):
-    s = input()
-    for j in range(len(s)):
-        if j == 0:
-            pass
-        elif s[j] == s[j-1]:
-            word_cnt += 1
-        elif s[j] not in word:
-            word_cnt += 1
-        word += s[j]
-    if word_cnt == len(s)-1:
-        cnt += 1
-    word_cnt = 0
-    word = ""
-print(cnt)
+print(length)
