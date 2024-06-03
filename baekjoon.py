6d4b5e684704cd1ba1aa3a0bcd3c2cbd763b1da1
tpqls774 <tpqls774@dgsw.hs.kr>
2024-06-03 19:33:11 +0900

    baekjoon.11650번[좌표 정렬하기]

--- baekjoon.py
+++ baekjoon.py
@@ -1,9 +1,7 @@
-from collections import deque
 n = int(input())
-card_li = [i for i in range(1, n+1)]
-card = deque(card_li)
-for i in range(n-1):
-    card.remove(card[0])
-    card.rotate(-1)
-result = list(card)
-print(*result)
+dt = []
+for i in range(n):
+    dt.append(list(map(int, input().split())))
+dt.sort()
+for i in range(n):
+    print(*dt[i])
